fabeecbe324f496d88253ddb47a7a1b892bfc366
Lucas Duarte <lucs.rkt@gmail.com>
2025-01-26 20:24:30 -0300

    feat: create services structure

---
+++ app/services/company_service.rb
@@ -0,0 +1,10 @@
+class CompanyService
+  def get_all_companies
+  end
+
+  def get_all_company(company_id)
+  end
+
+  def create_company(company_params)
+  end
+end
---
+++ app/services/employee_service.rb
@@ -0,0 +1,10 @@
+class EmployeeService
+  def get_employees_by_company(company_id)
+  end
+
+  def create_employee_by_company(company_id, employee_params)
+  end
+
+  def destroy_employee(employee_id)
+  end
+end
