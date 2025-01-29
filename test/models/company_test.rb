584762804d71750632796754e91f5926aa3b845b
Lucas Duarte <lucs.rkt@gmail.com>
2025-01-29 20:13:46 -0300

    test: company model

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -1,7 +1,27 @@
-require "test_helper"
-
 class CompanyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save company without a name" do
+    company = Company.new
+    assert_not company.save, "Saved the company without a name"
+  end
+
+  test "should save company with a name" do
+    company = Company.new(name: "Test Company")
+    assert company.save, "Failed to save the company with a name"
+  end
+
+  test "employees_balance should return correct counts" do
+    company = Company.create(name: "Test Company")
+
+    employee1 = Employee.create(name: "Employee 1", email: "email1", manager_id: nil, company_id: company.id)
+    Employee.create(name: "Employee 2", email: "email2", manager_id: employee1.id, company_id: company.id)
+    Employee.create(name: "Employee 3", email: "email3", manager_id: nil, company_id: company.id)
+
+    company.reload
+
+    balance = company.employees_balance
+
+    assert_equal 3, balance[:total_employees], "Total employees count is incorrect"
+    assert_equal 2, balance[:leaders_count], "Leaders count is incorrect"
+    assert_equal 1, balance[:followers_count], "Followers count is incorrect"
+  end
 end
