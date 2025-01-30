9ff43a109da83b93d8623d5f7cad27cea2ef4e25
Lucas Duarte <lucs.rkt@gmail.com>
2025-01-30 12:37:52 -0300

    feat: add seeds rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,35 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Company.destroy_all
+Employee.destroy_all
+
+company1 = Company.create!(name: "Empresa A")
+company2 = Company.create!(name: "Empresa B")
+
+employees_company1 = []
+10.times do |i|
+  employees_company1 << Employee.create!(
+    name: "Funcionário A#{i + 1}",
+    email: "funcionario_a#{i + 1}@empresa_a.com",
+    company: company1
+  )
+end
+
+employees_company2 = []
+10.times do |i|
+  employees_company2 << Employee.create!(
+    name: "Funcionário B#{i + 1}",
+    email: "funcionario_b#{i + 1}@empresa_b.com",
+    company: company2
+  )
+end
+
+employees_company1[0].update(manager: employees_company1[1])
+employees_company1[2].update(manager: employees_company1[1])
+employees_company1[3].update(manager: employees_company1[4])
+employees_company1[5].update(manager: employees_company1[4])
+
+employees_company2[0].update(manager: employees_company2[1])
+employees_company2[2].update(manager: employees_company2[1])
+employees_company2[3].update(manager: employees_company2[4])
+employees_company2[5].update(manager: employees_company2[4])
+
+puts "Seed concluído com sucesso!"
