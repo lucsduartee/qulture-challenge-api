7350d2e0ef7f353a2abd0b1e7c346f8be614fcd9
Lucas Duarte <lucs.rkt@gmail.com>
2025-01-30 13:33:30 -0300

    feat: add cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000" # Origem permitida (frontend)
+    origins "http://localhost:3000", "https://qulture-challenge-client.vercel.app"
 
     resource "*",
       headers: :any,
